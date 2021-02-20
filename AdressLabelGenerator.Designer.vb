b4f38d2c0baac0fc3a342d8afb8e194791fa4aa9
Taylor Herndon <TaylorBryanHerndon@gmail.com>
2021-02-19 21:40:53 -0700

    Add project files.

---
+++ AdressLabelGenerator.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class AdressLabelGenerator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(AdressLabelGenerator))
+        Me.MailingAdress = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.MITextBox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.FirstNameTextBox = New System.Windows.Forms.TextBox()
+        Me.ZipCodeTextBox = New System.Windows.Forms.TextBox()
+        Me.StateTextBox = New System.Windows.Forms.TextBox()
+        Me.CityTextBox = New System.Windows.Forms.TextBox()
+        Me.StreetAdressTextBox = New System.Windows.Forms.TextBox()
+        Me.LastNameTextBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.AdressLabel = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.SubmitButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MailingAdress
+        '
+        resources.ApplyResources(Me.MailingAdress, "MailingAdress")
+        Me.MailingAdress.Name = "MailingAdress"
+        '
+        'Panel1
+        '
+        resources.ApplyResources(Me.Panel1, "Panel1")
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.MailingAdress)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.MITextBox)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.FirstNameTextBox)
+        Me.Panel1.Controls.Add(Me.ZipCodeTextBox)
+        Me.Panel1.Controls.Add(Me.StateTextBox)
+        Me.Panel1.Controls.Add(Me.CityTextBox)
+        Me.Panel1.Controls.Add(Me.StreetAdressTextBox)
+        Me.Panel1.Controls.Add(Me.LastNameTextBox)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Name = "Panel1"
+        '
+        'Label7
+        '
+        resources.ApplyResources(Me.Label7, "Label7")
+        Me.Label7.Name = "Label7"
+        '
+        'Label6
+        '
+        resources.ApplyResources(Me.Label6, "Label6")
+        Me.Label6.Name = "Label6"
+        '
+        'Label5
+        '
+        resources.ApplyResources(Me.Label5, "Label5")
+        Me.Label5.Name = "Label5"
+        '
+        'Label4
+        '
+        resources.ApplyResources(Me.Label4, "Label4")
+        Me.Label4.Name = "Label4"
+        '
+        'Label3
+        '
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.Label3.Name = "Label3"
+        '
+        'MITextBox
+        '
+        resources.ApplyResources(Me.MITextBox, "MITextBox")
+        Me.MITextBox.Name = "MITextBox"
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Name = "Label2"
+        '
+        'FirstNameTextBox
+        '
+        resources.ApplyResources(Me.FirstNameTextBox, "FirstNameTextBox")
+        Me.FirstNameTextBox.Name = "FirstNameTextBox"
+        '
+        'ZipCodeTextBox
+        '
+        resources.ApplyResources(Me.ZipCodeTextBox, "ZipCodeTextBox")
+        Me.ZipCodeTextBox.Name = "ZipCodeTextBox"
+        '
+        'StateTextBox
+        '
+        resources.ApplyResources(Me.StateTextBox, "StateTextBox")
+        Me.StateTextBox.Name = "StateTextBox"
+        '
+        'CityTextBox
+        '
+        resources.ApplyResources(Me.CityTextBox, "CityTextBox")
+        Me.CityTextBox.Name = "CityTextBox"
+        '
+        'StreetAdressTextBox
+        '
+        resources.ApplyResources(Me.StreetAdressTextBox, "StreetAdressTextBox")
+        Me.StreetAdressTextBox.Name = "StreetAdressTextBox"
+        '
+        'LastNameTextBox
+        '
+        resources.ApplyResources(Me.LastNameTextBox, "LastNameTextBox")
+        Me.LastNameTextBox.Name = "LastNameTextBox"
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'Panel2
+        '
+        resources.ApplyResources(Me.Panel2, "Panel2")
+        Me.Panel2.Controls.Add(Me.AdressLabel)
+        Me.Panel2.Controls.Add(Me.Label8)
+        Me.Panel2.Name = "Panel2"
+        '
+        'AdressLabel
+        '
+        resources.ApplyResources(Me.AdressLabel, "AdressLabel")
+        Me.AdressLabel.Name = "AdressLabel"
+        '
+        'Label8
+        '
+        resources.ApplyResources(Me.Label8, "Label8")
+        Me.Label8.Name = "Label8"
+        '
+        'SubmitButton
+        '
+        resources.ApplyResources(Me.SubmitButton, "SubmitButton")
+        Me.SubmitButton.Name = "SubmitButton"
+        Me.SubmitButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        resources.ApplyResources(Me.ExitButton, "ExitButton")
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        resources.ApplyResources(Me.ClearButton, "ClearButton")
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'AdressLabelGenerator
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.SubmitButton)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "AdressLabelGenerator"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents MailingAdress As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LastNameTextBox As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents MITextBox As TextBox
+    Friend WithEvents FirstNameTextBox As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents StateTextBox As TextBox
+    Friend WithEvents CityTextBox As TextBox
+    Friend WithEvents StreetAdressTextBox As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents ZipCodeTextBox As TextBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents AdressLabel As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents SubmitButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ClearButton As Button
+End Class
---
+++ AdressLabelGenerator.vb
@@ -0,0 +1,152 @@
+﻿Option Strict On
+Option Explicit On
+
+'Taylor Herndon
+'RCET0265
+'Spring 2021
+'Adress Label Program
+'
+
+Public Class AdressLabelGenerator
+
+    Sub SubmitButtonPress() Handles SubmitButton.MouseDown
+
+        'Check to see if any of the folling text boxes contain a number or are empty
+        CheckString(FirstNameTextBox.Text, "First Name")
+        CheckString(MITextBox.Text, "Middle Inital")
+        CheckString(LastNameTextBox.Text, "Last Name")
+        CheckString(CityTextBox.Text, "City")
+        CheckString(StateTextBox.Text, "State")
+
+        'Check to see if the street adress text box is empty
+        If StreetAdressTextBox.Text = "" Then
+
+            StoreMessage("Street Adress", False)
+
+        End If
+
+        'Check to see if the zipcode text box contains only numbers
+        Try
+            'Try to conver to integer
+            Convert.ToInt32(ZipCodeTextBox.Text)
+
+        Catch ex As Exception
+
+            'If ZipCode cannot be converted to an integer there is a problem.
+
+            'Check to see if Zip Code is empty and store the correct response.
+            If ZipCodeTextBox.Text = "" Then
+
+                StoreMessage("Zip Code is empty", False)
+
+            Else
+
+                StoreMessage("Zip Code", False)
+
+            End If
+
+        End Try
+
+        'If there are any stored messages write them out in a msg box
+        If StoreMessage("", False) <> "" Then
+
+            MsgBox("An error occoured in the following fields." & vbNewLine & StoreMessage("", False))
+            StoreMessage("", True)
+            Exit Sub
+
+        End If
+
+        'If all tests are passed write out the adress label
+        AdressLabel.Text = FirstNameTextBox.Text & " " & MITextBox.Text & " " & LastNameTextBox.Text & vbNewLine &
+                           StreetAdressTextBox.Text & vbNewLine &
+                           CityTextBox.Text & ", " & StateTextBox.Text & " " & ZipCodeTextBox.Text
+
+    End Sub
+
+    Sub ClearButtonClick() Handles ClearButton.MouseDown
+
+        'Return adress label to default state
+        AdressLabel.Text = "Name " & vbNewLine & "Street Adress" & vbNewLine & "City State, Zip"
+
+        'Clear all text boxes
+        FirstNameTextBox.Text = ""
+        MITextBox.Text = ""
+        LastNameTextBox.Text = ""
+        StreetAdressTextBox.Text = ""
+        CityTextBox.Text = ""
+        StateTextBox.Text = ""
+        ZipCodeTextBox.Text = ""
+
+    End Sub
+
+    Sub ExitButtonClick() Handles ExitButton.MouseDown
+
+        End
+
+    End Sub
+
+    Function CheckString(CheckThisString As String, TestedField As String) As String
+
+        Dim StringLength As Integer
+        Dim HasNumber As String = "False"
+
+        'Gets the length of the string in question
+        StringLength = Len(CheckThisString)
+
+        'If the string is empty then return "Empty"
+        If CheckThisString = "" Then
+
+            StoreMessage(TestedField & " is empty.", False)
+            Return "Empty"
+
+        Else
+
+            'If the string is not empty test if each character is a number
+            For i = 0 To StringLength - 1
+
+                Try
+
+                    Convert.ToInt32(CheckThisString.Substring(i, 1)) 'Test the character
+                    HasNumber = "True" 'If the code continues then the tested character is a number
+                    StoreMessage(TestedField, False) 'Store what field has a problem
+
+                Catch ex As Exception
+
+                End Try
+
+            Next
+
+        End If
+
+        'Return whether or not the tested string has a number in it
+        Return HasNumber
+
+    End Function
+
+    Function StoreMessage(Message As String, Clear As Boolean) As String
+
+        Static StoredMessages As String
+
+        'If clear is true reset stored messages
+        If Clear Then
+
+            StoredMessages = ""
+            Return StoredMessages
+
+        End If
+
+        'If message is empty then return the stored messages and continue
+        If Message = "" Then
+
+            Return StoredMessages
+
+        End If
+
+        'Add the new message to the StoredMessages String
+        StoredMessages = StoredMessages & vbNewLine & Message
+
+        Return StoredMessages
+
+    End Function
+
+End Class
