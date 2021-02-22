24f4185a0ce4f590d8295cf8bac3f8e09ffea7a9
Taylor Herndon <TaylorBryanHerndon@gmail.com>
2021-02-22 08:25:07 -0700

    Fixed Tab Order and added accept and cancel buttons

--- AdressLabelGenerator.Designer.vb
+++ AdressLabelGenerator.Designer.vb
@@ -23,132 +23,35 @@ Partial Class AdressLabelGenerator
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(AdressLabelGenerator))
-        Me.MailingAdress = New System.Windows.Forms.Label()
-        Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Label7 = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.MITextBox = New System.Windows.Forms.TextBox()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.FirstNameTextBox = New System.Windows.Forms.TextBox()
-        Me.ZipCodeTextBox = New System.Windows.Forms.TextBox()
-        Me.StateTextBox = New System.Windows.Forms.TextBox()
-        Me.CityTextBox = New System.Windows.Forms.TextBox()
-        Me.StreetAdressTextBox = New System.Windows.Forms.TextBox()
-        Me.LastNameTextBox = New System.Windows.Forms.TextBox()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.Panel2 = New System.Windows.Forms.Panel()
         Me.AdressLabel = New System.Windows.Forms.Label()
         Me.Label8 = New System.Windows.Forms.Label()
         Me.SubmitButton = New System.Windows.Forms.Button()
         Me.ExitButton = New System.Windows.Forms.Button()
         Me.ClearButton = New System.Windows.Forms.Button()
-        Me.Panel1.SuspendLayout()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LastNameTextBox = New System.Windows.Forms.TextBox()
+        Me.StreetAdressTextBox = New System.Windows.Forms.TextBox()
+        Me.CityTextBox = New System.Windows.Forms.TextBox()
+        Me.StateTextBox = New System.Windows.Forms.TextBox()
+        Me.ZipCodeTextBox = New System.Windows.Forms.TextBox()
+        Me.FirstNameTextBox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.MITextBox = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.MailingAdress = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
         Me.Panel2.SuspendLayout()
         Me.SuspendLayout()
         '
-        'MailingAdress
-        '
-        resources.ApplyResources(Me.MailingAdress, "MailingAdress")
-        Me.MailingAdress.Name = "MailingAdress"
-        '
-        'Panel1
-        '
-        resources.ApplyResources(Me.Panel1, "Panel1")
-        Me.Panel1.Controls.Add(Me.Label7)
-        Me.Panel1.Controls.Add(Me.MailingAdress)
-        Me.Panel1.Controls.Add(Me.Label6)
-        Me.Panel1.Controls.Add(Me.Label5)
-        Me.Panel1.Controls.Add(Me.Label4)
-        Me.Panel1.Controls.Add(Me.Label3)
-        Me.Panel1.Controls.Add(Me.MITextBox)
-        Me.Panel1.Controls.Add(Me.Label2)
-        Me.Panel1.Controls.Add(Me.FirstNameTextBox)
-        Me.Panel1.Controls.Add(Me.ZipCodeTextBox)
-        Me.Panel1.Controls.Add(Me.StateTextBox)
-        Me.Panel1.Controls.Add(Me.CityTextBox)
-        Me.Panel1.Controls.Add(Me.StreetAdressTextBox)
-        Me.Panel1.Controls.Add(Me.LastNameTextBox)
-        Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Name = "Panel1"
-        '
-        'Label7
-        '
-        resources.ApplyResources(Me.Label7, "Label7")
-        Me.Label7.Name = "Label7"
-        '
-        'Label6
-        '
-        resources.ApplyResources(Me.Label6, "Label6")
-        Me.Label6.Name = "Label6"
-        '
-        'Label5
-        '
-        resources.ApplyResources(Me.Label5, "Label5")
-        Me.Label5.Name = "Label5"
-        '
-        'Label4
-        '
-        resources.ApplyResources(Me.Label4, "Label4")
-        Me.Label4.Name = "Label4"
-        '
-        'Label3
-        '
-        resources.ApplyResources(Me.Label3, "Label3")
-        Me.Label3.Name = "Label3"
-        '
-        'MITextBox
-        '
-        resources.ApplyResources(Me.MITextBox, "MITextBox")
-        Me.MITextBox.Name = "MITextBox"
-        '
-        'Label2
-        '
-        resources.ApplyResources(Me.Label2, "Label2")
-        Me.Label2.Name = "Label2"
-        '
-        'FirstNameTextBox
-        '
-        resources.ApplyResources(Me.FirstNameTextBox, "FirstNameTextBox")
-        Me.FirstNameTextBox.Name = "FirstNameTextBox"
-        '
-        'ZipCodeTextBox
-        '
-        resources.ApplyResources(Me.ZipCodeTextBox, "ZipCodeTextBox")
-        Me.ZipCodeTextBox.Name = "ZipCodeTextBox"
-        '
-        'StateTextBox
-        '
-        resources.ApplyResources(Me.StateTextBox, "StateTextBox")
-        Me.StateTextBox.Name = "StateTextBox"
-        '
-        'CityTextBox
-        '
-        resources.ApplyResources(Me.CityTextBox, "CityTextBox")
-        Me.CityTextBox.Name = "CityTextBox"
-        '
-        'StreetAdressTextBox
-        '
-        resources.ApplyResources(Me.StreetAdressTextBox, "StreetAdressTextBox")
-        Me.StreetAdressTextBox.Name = "StreetAdressTextBox"
-        '
-        'LastNameTextBox
-        '
-        resources.ApplyResources(Me.LastNameTextBox, "LastNameTextBox")
-        Me.LastNameTextBox.Name = "LastNameTextBox"
-        '
-        'Label1
-        '
-        resources.ApplyResources(Me.Label1, "Label1")
-        Me.Label1.Name = "Label1"
-        '
         'Panel2
         '
-        resources.ApplyResources(Me.Panel2, "Panel2")
         Me.Panel2.Controls.Add(Me.AdressLabel)
         Me.Panel2.Controls.Add(Me.Label8)
+        resources.ApplyResources(Me.Panel2, "Panel2")
         Me.Panel2.Name = "Panel2"
         '
         'AdressLabel
@@ -179,44 +82,132 @@ Partial Class AdressLabelGenerator
         Me.ClearButton.Name = "ClearButton"
         Me.ClearButton.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'LastNameTextBox
+        '
+        resources.ApplyResources(Me.LastNameTextBox, "LastNameTextBox")
+        Me.LastNameTextBox.Name = "LastNameTextBox"
+        '
+        'StreetAdressTextBox
+        '
+        resources.ApplyResources(Me.StreetAdressTextBox, "StreetAdressTextBox")
+        Me.StreetAdressTextBox.Name = "StreetAdressTextBox"
+        '
+        'CityTextBox
+        '
+        resources.ApplyResources(Me.CityTextBox, "CityTextBox")
+        Me.CityTextBox.Name = "CityTextBox"
+        '
+        'StateTextBox
+        '
+        resources.ApplyResources(Me.StateTextBox, "StateTextBox")
+        Me.StateTextBox.Name = "StateTextBox"
+        '
+        'ZipCodeTextBox
+        '
+        resources.ApplyResources(Me.ZipCodeTextBox, "ZipCodeTextBox")
+        Me.ZipCodeTextBox.Name = "ZipCodeTextBox"
+        '
+        'FirstNameTextBox
+        '
+        resources.ApplyResources(Me.FirstNameTextBox, "FirstNameTextBox")
+        Me.FirstNameTextBox.Name = "FirstNameTextBox"
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Name = "Label2"
+        '
+        'MITextBox
+        '
+        resources.ApplyResources(Me.MITextBox, "MITextBox")
+        Me.MITextBox.Name = "MITextBox"
+        '
+        'Label3
+        '
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.Label3.Name = "Label3"
+        '
+        'Label4
+        '
+        resources.ApplyResources(Me.Label4, "Label4")
+        Me.Label4.Name = "Label4"
+        '
+        'Label5
+        '
+        resources.ApplyResources(Me.Label5, "Label5")
+        Me.Label5.Name = "Label5"
+        '
+        'Label6
+        '
+        resources.ApplyResources(Me.Label6, "Label6")
+        Me.Label6.Name = "Label6"
+        '
+        'MailingAdress
+        '
+        resources.ApplyResources(Me.MailingAdress, "MailingAdress")
+        Me.MailingAdress.Name = "MailingAdress"
+        '
+        'Label7
+        '
+        resources.ApplyResources(Me.Label7, "Label7")
+        Me.Label7.Name = "Label7"
+        '
         'AdressLabelGenerator
         '
+        Me.AcceptButton = Me.SubmitButton
         resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.ExitButton
+        Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.MailingAdress)
         Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.SubmitButton)
+        Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Panel2)
-        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MITextBox)
+        Me.Controls.Add(Me.LastNameTextBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.StreetAdressTextBox)
+        Me.Controls.Add(Me.FirstNameTextBox)
+        Me.Controls.Add(Me.CityTextBox)
+        Me.Controls.Add(Me.ZipCodeTextBox)
+        Me.Controls.Add(Me.StateTextBox)
         Me.Name = "AdressLabelGenerator"
-        Me.Panel1.ResumeLayout(False)
-        Me.Panel1.PerformLayout()
         Me.Panel2.ResumeLayout(False)
         Me.Panel2.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents MailingAdress As Label
-    Friend WithEvents Panel1 As Panel
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents LastNameTextBox As TextBox
-    Friend WithEvents Label3 As Label
-    Friend WithEvents MITextBox As TextBox
-    Friend WithEvents FirstNameTextBox As TextBox
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Label5 As Label
-    Friend WithEvents StateTextBox As TextBox
-    Friend WithEvents CityTextBox As TextBox
-    Friend WithEvents StreetAdressTextBox As TextBox
-    Friend WithEvents Label7 As Label
-    Friend WithEvents Label6 As Label
-    Friend WithEvents ZipCodeTextBox As TextBox
     Friend WithEvents Panel2 As Panel
     Friend WithEvents AdressLabel As Label
     Friend WithEvents Label8 As Label
     Friend WithEvents SubmitButton As Button
     Friend WithEvents ExitButton As Button
     Friend WithEvents ClearButton As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LastNameTextBox As TextBox
+    Friend WithEvents StreetAdressTextBox As TextBox
+    Friend WithEvents CityTextBox As TextBox
+    Friend WithEvents StateTextBox As TextBox
+    Friend WithEvents ZipCodeTextBox As TextBox
+    Friend WithEvents FirstNameTextBox As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents MITextBox As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents MailingAdress As Label
+    Friend WithEvents Label7 As Label
 End Class
